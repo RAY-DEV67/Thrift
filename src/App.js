484af27ce1354c37cd29a0c97e35218a3248185c
Elebor Henry <henryekene8@gmail.com>
2023-02-03 11:41:22 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar } from "./components/navbar";
-import { Home } from "./components/home";
-import { useState } from "react";
-import { Destination } from "./components/destination";
-import { Crew } from "./components/crew";
-import { useLocation } from "react-router-dom";
-import { Technology } from "./components/technology";
-import { motion, AnimateSharedLayout } from "framer-motion";
+
+
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/destination" element={<Destination />} />
-          <Route path="/crew" element={<Crew />} />
-          <Route path="/technology" element={<Technology />} />
-        </Routes>
-      </Router>
+    <h1 className="mt-[12rem]">Thrifty</h1>
     </div>
   );
 }
